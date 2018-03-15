0665c02b071f9500258c21cea652be01ccd8c287
东往 <wgzhxy@163.com>
2018-03-15 12:44:18 +0800

    配置更新

--- starter/src/main/java/com/yisutech/iisp/starter/StarterApplication.java
+++ starter/src/main/java/com/yisutech/iisp/starter/StarterApplication.java
@@ -2,11 +2,13 @@ package com.yisutech.iisp.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.yisutech.iisp")
 public class StarterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StarterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApplication.class, args);
+    }
 }
