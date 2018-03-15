d2871a2824cbea93928f533f56f1760d1d4d0a11
东往 <wgzhxy@163.com>
2018-03-16 00:22:56 +0800

    增加dataops,search模块

---
+++ iisp_dataops/src/main/java/com/yisutech/iisp/base_dataops/YisutechCoreEngineApplication.java
@@ -0,0 +1,12 @@
+package com.yisutech.iisp.base_dataops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YisutechCoreEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YisutechCoreEngineApplication.class, args);
+	}
+}
---
+++ iisp_search/src/main/java/com/yisutech/iisp/base_seach/SearchEngineApplication.java
@@ -0,0 +1,12 @@
+package com.yisutech.iisp.base_seach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchEngineApplication.class, args);
+	}
+}
