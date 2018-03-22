2baafb5a57ab5e6396cbd35f8b5cc79916cbb4b9
东往 <wgzhxy@163.com>
2018-03-22 19:39:08 +0800

    配置数据源

--- starter/src/main/java/com/yisutech/iisp/starter/StarterApplication.java
+++ starter/src/main/java/com/yisutech/iisp/starter/StarterApplication.java
@@ -1,11 +1,15 @@
 package com.yisutech.iisp.starter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
+@MapperScan("com.yisutech.iisp.dataops.repository.mapper")
 @ComponentScan("com.yisutech.iisp")
 public class StarterApplication {
 
