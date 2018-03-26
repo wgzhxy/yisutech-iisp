dd509e09d93aa607a59bd66e6fd5a448e1766588
东往 <wgzhxy@163.com>
2018-03-26 14:49:37 +0800

    注释完善

--- iisp-dataops/src/main/java/com/yisutech/iisp/dataops/engine/template/MongoDBTemplate.java
+++ iisp-dataops/src/main/java/com/yisutech/iisp/dataops/engine/template/MongoDBTemplate.java
@@ -4,11 +4,13 @@ import com.yisutech.iisp.dataops.engine.DataOps;
 
 /**
  * 版权：Copyright by www.yisutech.com
- * 文件名：com.yisutech.iisp.dataops.engine.template.MongDBTemplate
+ * 文件名：com.yisutech.iisp.dataops.engine.template.MongoDBTemplate
  * 描述：
+ * <p>
+ * MongoDB操作模板定义
  *
  * @author guangzhong.wgz
  * @create 2018-03-26 下午2:45
  **/
-public interface MongDBTemplate extends DataOps {
+public interface MongoDBTemplate extends DataOps {
 }
---
+++ iisp-dataops/src/main/java/com/yisutech/iisp/dataops/engine/template/impl/MongoDBTemplateImpl.java
@@ -0,0 +1,17 @@
+package com.yisutech.iisp.dataops.engine.template.impl;
+
+import com.yisutech.iisp.dataops.engine.template.MongoDBTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * 版权：Copyright by www.yisutech.com
+ * 文件名：com.yisutech.iisp.dataops.engine.template.impl.MongoDBTemplateImpl
+ * 描述：
+ * <p>
+ * MongoDB操作模板实现
+ *
+ * @author guangzhong.wgz
+ * @create 2018-03-26 下午2:46
+ **/
+public class MongoDBTemplateImpl implements MongoDBTemplate {
+}
