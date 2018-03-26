4deebbf9f791f5adc1c0b658bdeb453208e132f5
东往 <wgzhxy@163.com>
2018-03-26 10:26:32 +0800

    完善注释

--- iisp-dataops/src/main/java/com/yisutech/iisp/dataops/engine/DataOpsTemplate.java
+++ iisp-dataops/src/main/java/com/yisutech/iisp/dataops/engine/DataOpsTemplate.java
@@ -18,74 +18,117 @@ public interface DataOpsTemplate {
 
     /**
      * 创建表
+     *
+     * @param tableMeta {@link TableMeta}
+     * @return true, 成功; false, 失败
      */
     boolean createTable(TableMeta tableMeta);
 
     /**
      * 删除表
+     *
+     * @param tableMeta {@link TableMeta}
+     * @return true, 成功; false, 失败
      */
     boolean dropTable(TableMeta tableMeta);
 
     /**
      * 变更新
+     *
+     * @param tableMeta {@link TableMeta}
+     * @return true, 成功; false, 失败
      */
     boolean alterTable(TableMeta tableMeta, Map<String, ColumnMeta> columnMetas);
 
     /**
      * 查询表
      *
-     * @return
+     * @param tableMetas {@link TableMeta}
+     * @return List<Map<String, Object>>
      */
     List<Map<String, Object>> query(List<TableMeta> tableMetas);
 
     /**
      * 查询表
      *
-     * @return
+     * @param tableMetas {@link TableMeta}
+     * @param offset     开始记录
+     * @param size       记录数
+     * @return List<Map<String, Object>>
      */
     List<Map<String, Object>> query(List<TableMeta> tableMetas, int offset, int size);
 
     /**
      * 查询表
      *
-     * @return
+     * @param sqlTemplate sql模板(freemarker模板)
+     * @param values      Map<String, Object> 模板内动态变量
+     * @return List<Map<String, Object>>
      */
     List<Map<String, Object>> query(String sqlTemplate, Map<String, Object> values);
 
     /**
      * 查询表
      *
-     * @return
+     * @param sqlTemplate sql模板(freemarker模板)
+     * @param values      Map<String, Object> 模板内动态变量
+     * @param offset      记录开始位置
+     * @param size        记录数
+     * @return List<Map<String, Object>>
      */
     List<Map<String, Object>> query(String sqlTemplate, Map<String, Object> values, int offset, int size);
 
     /**
      * 写入记录
+     *
+     * @param tableMeta 表元信息
+     * @param values    值集合
+     * @return 主键ID
      */
     int insert(TableMeta tableMeta, Map<String, Object> values);
 
     /**
      * 写入记录
+     *
+     * @param sqlTemplate sql模板(freemarker)
+     * @param values      值集合
+     * @return 主键ID
      */
     int insert(String sqlTemplate, Map<String, Object> values);
 
     /**
-     * 写入记录
+     * 批量写入记录
+     *
+     * @param sqlTemplate sql模板
+     * @param values      值集合
+     * @return 主键集合
      */
     List<Integer> batchInsert(String sqlTemplate, List<Map<String, Object>> values);
 
     /**
      * 更新记录
+     *
+     * @param tableMeta 表元信息
+     * @param values    值集合
+     * @return 记录数
      */
     int update(TableMeta tableMeta, Map<String, Object> values);
 
     /**
      * 更新记录
+     *
+     * @param sqlTemplate sql模板
+     * @param values      值集合
+     * @return 记录数
      */
     int update(String sqlTemplate, Map<String, Object> values);
 
     /**
-     * 更新记录
+     * 批量更新记录
+     *
+     * @param sqlTemplate sql模板
+     * @param values      值集合
+     * @return 记录数
      */
     List<Integer> batchUpdate(String sqlTemplate, List<Map<String, Object>> values);
 }
