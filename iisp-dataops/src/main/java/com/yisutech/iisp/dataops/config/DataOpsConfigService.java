7272780e869ff36c5830c376ed1e08413856c60e
东往 <wgzhxy@163.com>
2018-03-28 20:39:37 +0800

    基础数据配置服务定义

---
+++ iisp-dataops/src/main/java/com/yisutech/iisp/dataops/config/DataOpsConfigService.java
@@ -0,0 +1,35 @@
+package com.yisutech.iisp.dataops.config;
+
+import com.yisutech.iisp.dataops.engine.template.model.TableMeta;
+
+import java.util.List;
+
+/**
+ * 版权：Copyright by www.yisutech.com
+ * 文件名：com.yisutech.iisp.dataops.config.DataOpsConfigService
+ * 描述：
+ * 数据操作基础配置
+ *
+ * @author guangzhong.wgz
+ * @create 2018-03-28 下午7:37
+ **/
+public interface DataOpsConfigService {
+
+    /**
+     * 获取表元信息
+     *
+     * @param tableCode 表编码
+     * @return
+     */
+    TableMeta getTableMeta(String tableCode);
+
+    /**
+     * 批量获取表元信息
+     *
+     * @param tableCodes 表编码集合
+     * @return
+     */
+    TableMeta getTableMetas(List<String> tableCodes);
+
+
+}
---
+++ iisp-dataops/src/main/java/com/yisutech/iisp/dataops/config/impl/DataOpsConfigServiceImpl.java
@@ -0,0 +1,30 @@
+package com.yisutech.iisp.dataops.config.impl;
+
+import com.yisutech.iisp.dataops.config.DataOpsConfigService;
+import com.yisutech.iisp.dataops.engine.template.model.TableMeta;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 版权：Copyright by www.yisutech.com
+ * 文件名：com.yisutech.iisp.dataops.config.impl.DataOpsConfigServiceImpl
+ * 描述：
+ * 数据操作基础配置服务
+ *
+ * @author guangzhong.wgz
+ * @create 2018-03-28 下午7:38
+ **/
+@Service
+public class DataOpsConfigServiceImpl implements DataOpsConfigService {
+
+    @Override
+    public TableMeta getTableMeta(String tableCode) {
+        return null;
+    }
+
+    @Override
+    public TableMeta getTableMetas(List<String> tableCodes) {
+        return null;
+    }
+}
