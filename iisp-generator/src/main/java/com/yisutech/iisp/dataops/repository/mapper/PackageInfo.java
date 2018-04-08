52d1e45e0aa445c71f7401dd8ec1f3dfb6d90488
东往 <wgzhxy@163.com>
2018-04-08 17:45:08 +0800

    目标提交

---
+++ iisp-generator/src/main/java/com/yisutech/iisp/dataops/repository/mapper/PackageInfo.java
@@ -0,0 +1,12 @@
+package com.yisutech.iisp.dataops.repository.mapper;
+
+/**
+ * 版权：Copyright by www.yisutech.com
+ * 文件名：com.yisutech.iisp.dataops.repository.mapper.PackageInfo
+ * 描述：
+ *
+ * @author guangzhong.wgz
+ * @create 2018-04-08 下午5:44
+ **/
+public class PackageInfo {
+}
---
+++ iisp-generator/src/main/java/com/yisutech/iisp/dataops/repository/pojo/PakcageInfo.java
@@ -0,0 +1,12 @@
+package com.yisutech.iisp.dataops.repository.pojo;
+
+/**
+ * 版权：Copyright by www.yisutech.com
+ * 文件名：com.yisutech.iisp.dataops.repository.pojo.PakcageInfo
+ * 描述：
+ *
+ * @author guangzhong.wgz
+ * @create 2018-04-08 下午5:44
+ **/
+public class PakcageInfo {
+}
