ccf514933288adc693bf0d596f8cd3ca81754ad3
东往 <wgzhxy@163.com>
2018-04-13 16:11:33 +0800

    增加声明性事务

--- starter/src/main/java/com/yisutech/iisp/starter/StarterApplication.java
+++ starter/src/main/java/com/yisutech/iisp/starter/StarterApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableTransactionManagement
 @MapperScan("com.yisutech.iisp.dataops.repository.mapper")
 @ComponentScan("com.yisutech.iisp")
 public class StarterApplication {
